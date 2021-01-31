9954e362a243adb21718a9afbe46f452133b9905
CorentinKowalsky <75315040+CorentinKowalsky@users.noreply.github.com>
2021-01-31 15:24:52 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("BJ")
+function hello(){
+    var element = document.querySelector('article')
+    element.classList.toggle('Hello')
+}
+function bonjour(){
+    var element = document.getElementById("second")
+    element.classList.toggle('Hello')
+}
