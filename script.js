83ca769075503baa42195c55d519f621a107c1a2
CorentinKowalsky <75315040+CorentinKowalsky@users.noreply.github.com>
2021-02-01 12:24:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function hello(){
-    var element = document.querySelector('article')
-    element.classList.toggle('Hello')
-}
 function bonjour(){
     var element = document.getElementById("second")
     element.classList.toggle('Hello')
